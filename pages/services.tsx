1128bea750964cefa58875cd94cedfbe1882555d
JaumDarkz <viniciuspkfr@gmail.com>
2022-07-30 11:24:05 -0300

    feat: base config

---
+++ pages/services.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Services: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>MetaMath - Services</title>
+        <meta name="description" content="Website description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+    </>
+  )
+}
+
+export default Services
